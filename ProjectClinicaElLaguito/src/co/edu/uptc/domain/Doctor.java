a8da5fa27580ca8c8ecaf8a733bf0e293b57fa7b
Sergio8D-holi <serfe.correa@gmail.com>
2026-04-30 22:54:10 -0500

    Se creo la clase Doctor sin documentar ninguna clase, toca documentarlo en una proxima version

---
+++ ProjectClinicaElLaguito/src/co/edu/uptc/domain/Doctor.java
@@ -0,0 +1,73 @@
+package co.edu.uptc.domain;
+
+import co.edu.uptc.enums.IdentificationTypeEnum;
+
+public class Doctor {
+	private IdentificationTypeEnum identificationType;
+	private int medicalId;
+	private String firstName;
+	private String lastName;
+	private String specialty;
+	private int yearsOfExperience;
+	
+	public Doctor(IdentificationTypeEnum identificationType, int medicalId, String firstName, String lastName,
+			String specialty, int yearsOfExperience) {
+		super();
+		this.identificationType = identificationType;
+		this.medicalId = medicalId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.specialty = specialty;
+		this.yearsOfExperience = yearsOfExperience;
+	}
+
+	public IdentificationTypeEnum getIdentificationType() {
+		return identificationType;
+	}
+
+	public void setIdentificationType(IdentificationTypeEnum identificationType) {
+		this.identificationType = identificationType;
+	}
+
+	public int getMedicalId() {
+		return medicalId;
+	}
+
+	public void setMedicalId(int medicalId) {
+		this.medicalId = medicalId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(String specialty) {
+		this.specialty = specialty;
+	}
+
+	public int getYearsOfExperience() {
+		return yearsOfExperience;
+	}
+
+	public void setYearsOfExperience(int yearsOfExperience) {
+		this.yearsOfExperience = yearsOfExperience;
+	}
+	
+	
+}	
